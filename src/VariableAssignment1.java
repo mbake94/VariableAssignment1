aa748b24d0abdebe0f710ee710e2ff27275c8d43
mbake94 <mbake94@gmail.com>
2020-04-14 08:42:43 -0700

    Variable Assignment 1

---
+++ src/VariableAssignment1.java
@@ -0,0 +1,11 @@
+public class VariableAssignment1 {
+    public static void main (String[] args) {
+        int testScore = 80;
+        double textBookCost = 498.39;
+        String studentName = "Miles Baker";
+
+        System.out.println("Grade: " + testScore);
+        System.out.println("Your books will cost: " + textBookCost);
+        System.out.println("Student: " + studentName);
+    }
+}
